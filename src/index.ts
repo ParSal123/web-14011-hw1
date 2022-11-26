6495c8c9718773e6f6aff9b1554feefb0860e46e
Parsa Salavati <parsa.salavati@gmail.com>
2022-11-26 14:49:16 +0330

    Add responsive design for login button

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import './static/css/main.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
 console.log("Hello World!");
